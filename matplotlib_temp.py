1f2e26f3a96e536e9995d31696910aa3b3a545b8
Puhtooie <44322930+Puhtooie@users.noreply.github.com>
2018-11-23 08:42:48 -0700

    Update matplotlib_temp.py

--- matplotlib_temp.py
+++ matplotlib_temp.py
@@ -3,11 +3,11 @@ from matplotlib import pyplot as plt
 from datetime import datetime
 
 def graph_test(dictionary=test):
-    for i in range(len(dictionary)):
-        data = dictionary[i]
+    for pandas_df in range(len(dictionary)):
+        data = dictionary[pandas_df]
         if data == 'user input':
-            time= np.array([dictionary[i][df][6] for df in range(len(dictionary[i]))])#the 3rd index [6] is the current index for time
-            parameter= np.array([dictionary[i][df][4] for df in range(len(dictionary[i]))])#[4] the data to measure parameter 
+            time= dictionary[pandas_df][0]#the 3rd index [6] is the current index for time
+            parameter= dictionary[pandas_df][1]#[4] the data to measure parameter 
             for t in range(len(time)):
                 time[t]= datetime.utcfromtimestamp(time[t] // 1000)
                 # high, low query to compute profit/loss
