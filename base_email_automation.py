e30718407c2127f5a9e1472415b5b8e9c11033c8
Puhtooie <44322930+Puhtooie@users.noreply.github.com>
2019-03-23 17:53:31 -0700

    Update base_email_automation.py

--- base_email_automation.py
+++ base_email_automation.py
@@ -7,47 +7,48 @@ from email.mime.base import MIMEBase
 from email.utils import formatdate
 from email import encoders
 
-def send_email(email='str',password='str', send_to='str',
-               subject='str',message='str',files=[False]):
-
-    msg=MIMEMultipart()
-    msg['From']=email
-    msg['To']=send_to_email
-    msg['Date']= formatdate(localtime=True)
-    msg['Subject']=subject
-
-    msg.attach(MIMEText, 'plain')
-    if files[0] != False:
-        for f in files or []:
-            with open(f, "rb") as fil:
-                part = MIMEApplication(
-                fil.read(),
-                Name=basename(f)
-                )  
-    # After the file is closed
-            part['Content-Disposition'] = 'attachment; filename="%s"' % basename(f)
-            msg.attach(part)
-
-
-    server=smtplib.SMTP('smtp.gmail.com',587)
-    server.starttls()
-    server.login(email,password)
-    server.sendmail(email,send_to,text)
-    server.quit()
+class Email()
+  def __init__(email, password, 
+               addresses, subject,
+               files = False, text):
+      this.email = email #sending email String
+      this.pass = password #sending email pass String
+      this.addresses = addresses
+      this.subject = subject #subject headed String
+      this.files = files #the location of files to be attatched List of Strings
+      this.text = text #text to be sent String
     
-def format_emails():
-    file_locations=['C:\\the_location_of\\your_file',
-                   'C:\\the_location_of\\your_2nd_file']
-    email='youremail@gmail.com'
-    pas='password'
-    subject='Subject title'
+  def send_email(sendTo):
 
-    addresses=['intended_emails@mail.com']
+      msg=MIMEMultipart()
+      msg['From']= this.email
+      msg['To']= sendTo
+      msg['Date']= formatdate(localtime=True)
+      msg['Subject']= this.subject
 
-    message='text or file upload'
+      msg.attach(MIMEText, 'plain')
+      if this.files != False:
+          for f in this.files or []:
+              with open(f, "rb") as fil:
+                  part = MIMEApplication(
+                  fil.read(),
+                  Name=basename(f)
+                  )  
+      # After the file is closed
+              part['Content-Disposition'] = 'attachment; filename="%s"' % basename(f)
+              msg.attach(part)
+      server.sendmail( this.email, sendTo, this.text)
 
-    for i in addresses:
-      #if you don't have any attatchments to send makes files=[False]
-      send_email(email=email,password=pas, send_to_email=i,
-                   subject=subject,message=message,files=file_locations)
-format_emails()
+
+
+  def format_emails():
+
+      server=smtplib.SMTP('smtp.gmail.com',587)
+      server.starttls()
+      server.login(this.email, this.password)
+
+      for i in this.addresses:
+          send_email(sendTo = i)
+      
+      server.quit()
+
