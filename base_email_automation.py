2b9f9271da96fbc15bfa115b2cf3a07760df9a23
Puhtooie <44322930+Puhtooie@users.noreply.github.com>
2019-07-19 18:12:52 -0700

    Update base_email_automation.py

--- base_email_automation.py
+++ base_email_automation.py
@@ -3,54 +3,65 @@ from os.path import basename
 from email.mime.text import MIMEText
 from email.mime.application import MIMEApplication
 from email.mime.multipart import MIMEMultipart
-from email.mime.base import MIMEBase
 from email.utils import formatdate
 from email import encoders
 
-class Email:
-  
-  
-  def __init__(email, password, 
-               addresses, subject,
-               files = False, text):
-      this.email = email #sending email String
-      this.pass = password #sending email pass String
-      this.addresses = addresses #List of Email Addresses in Strings
-      this.subject = subject #subject headed String
-      this.files = files #the location of files to be attatched List of Strings
-      this.text = text #text to be sent String
-    
-  def send_email( this, sendTo):
-
-      msg=MIMEMultipart()
-      msg['From']= this.email
-      msg['To']= sendTo
-      msg['Date']= formatdate(localtime=True)
-      msg['Subject']= this.subject
-
-      msg.attach(MIMEText, 'plain')
-      if this.files != False:
-          for f in this.files or []:
-              with open(f, "rb") as fil:
-                  part = MIMEApplication(
-                  fil.read(),
-                  Name=basename(f)
-                  )  
-      # After the file is closed
-              part['Content-Disposition'] = 'attachment; filename="%s"' % basename(f)
-              msg.attach(part)
-      server.sendmail( this.email, sendTo, this.text)
+'''
+Microsoft word= .docx
+OpenDocument Format=.odt
+Rich Text Format= .rft
+PDF Document= .pdf
+Plain Text= .txt
+change gmail to less secure
+'''
 
 
+class autoEmail:
 
-  def format_emails(this):
+    def __init__(self, email, pswd, subject, message, addresses,file_loc = 'na'):
 
-      server=smtplib.SMTP('smtp.gmail.com',587)
-      server.starttls()
-      server.login(this.email, this.password)
+        self.email = email
+        self.pswd = pswd
+        self.subj = subject
+        self.msg = message
+        #addresses in list 
+        self.adrs = addresses 
+        if file_loc != 'na':
+            self.files = [False]
+        else:
+            self.files= file_loc
 
-      for i in this.addresses:
-          send_email(sendTo = i)
-      
-      server.quit()
+    def send_email(self,send_to='str'):
+
+        msg=MIMEMultipart()
+        msg['From']=self.email
+        msg['To']=send_to
+        msg['Date']= formatdate(localtime=True)
+        msg['Subject']=self.subject
+
+        msg.attach(MIMEText(self.text, 'plain'))
+        if self.files[0] != False:
+            for f in self.files or []:
+                with open(f, "rb") as fil:
+                    part = MIMEApplication(
+                    fil.read(),
+                    Name=basename(f)
+                    )  
+        # After the file is closed
+                part['Content-Disposition'] = 'attachment; filename="%s"' % basename(f)
+                msg.attach(part)
+
+        text=msg.as_string()
+
+        server.sendmail(self.email,send_to,self.text)
+
+    def format_email():
+        server=smtplib.SMTP('smtp.gmail.com',587)
+        server.starttls()
+        server.login(self.email,self.pswd)
+
+        for i in self.adrs:
+            self.send_email(send_to=i)
+        server.quit()
+
 
